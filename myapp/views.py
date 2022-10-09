16daedde7dc4561f43619f0d36c336589cf2d268
Allu-Allus <alluajith29@gmail.com>
2022-10-10 01:20:10 +0530

    10-10-2022

--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,21 @@
+from multiprocessing import context
 from django.http import HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
 def index(request):
     d = {
         "name":"Allu",
         "age":"24" ,
     }
-    return HttpResponse("<b>Hello World</b>")
+    
+    li = ["Allen","Sreerag","Alwin","Allu"]
+
+    for i in range(0,10):
+        print(i)
+
+    context = {'names': li}
+
+    return render(request, 'myapp/index.html',context=context)
+
+def new_one(request):
+    return render(request, 'listing/new_one.html')
