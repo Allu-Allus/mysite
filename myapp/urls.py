8ac57f3d5a1339adb1da42a7fba408e54c86b353
Allu-Allus <alluajith29@gmail.com>
2022-10-14 22:19:08 +0530

    14-10-2022

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path,include
-from .views import index , new_one
+from .views import index , new_one, product_details, products
 
 urlpatterns = [
     path('',index),
-      path('new',new_one),
-     
-]
+      path('new/',new_one),
+      path('products/', products),
+     path('products/<int:id>',product_details)
+] 
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,10 @@
+from itertools import product
+from myapp.models import Product
 from cgitb import html
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+ 
 # Create your views here.
 def index(request):
     d = {
@@ -21,3 +24,13 @@ def index(request):
 def new_one(request):
     return render(request, 'listing/new_one.html')
 
+def products(request):
+    # p = Product.objects.filter(price__gt =100000) 
+    p = Product.objects.all()
+    context = {'products':p}
+    return  render(request, 'myapp/products.html',context=context)
+
+def product_details(request,id):
+    p = Product.objects.get(id=id)
+    context = {'p':p}
+    return render(request,'myapp/product_details.html',context=context)
